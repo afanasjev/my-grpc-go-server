b3aedf12022c993634cd0dba49d1c497a6eb55c6
Alexander Afanasjev <alexandr.afanasiev@aliexpress.ru>
2024-11-27 23:38:26 +0300

    init

---
+++ internal/adapter/grpc/server.go
@@ -0,0 +1,47 @@
+package grpc
+
+import (
+	"fmt"
+	"github.com/afanasjev/my-grpc-go-server/internal/port"
+	"github.com/afanasjev/proto-repo/protogen/go/hello"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type GrpcAdapter struct {
+	helloservice port.HelloServicePort
+	port         int
+	server       *grpc.Server
+	hello.HelloServiceServer
+}
+
+func NewGrpcAdapter(helloservice port.HelloServicePort, grpcPort int) *GrpcAdapter {
+	return &GrpcAdapter{
+		helloservice: helloservice,
+		port:         grpcPort,
+	}
+}
+
+func (s *GrpcAdapter) Run() {
+	var err error
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", s.port))
+
+	if err != nil {
+		log.Fatalf("failed to listen on port %v: %v", s.port, err)
+	}
+
+	log.Printf("failed to listen on port %v", s.port)
+
+	grpcServer := grpc.NewServer()
+	s.server = grpcServer
+	hello.RegisterHelloServiceServer(grpcServer, s)
+
+	if err := s.server.Serve(listen); err != nil {
+		log.Fatalf("failed to serve grpc on port %v: %v", s.port, err)
+	}
+}
+
+func (s *GrpcAdapter) Stop() {
+	s.server.GracefulStop()
+}
